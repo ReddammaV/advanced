8b46d98819777b1210ff70271742fc4418384789
reddamma <velasirireddamma@gmail.com>
2019-11-26 18:12:13 +0530

    added path delete src in the path

--- src/app/books/book-detail/book-detail.component.ts
+++ src/app/books/book-detail/book-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { BookService } from 'src/app/services/book.service';
-import { Book } from 'src/app/interfaces/book';
+import { BookService } from '../../services/book.service';
+import { Book } from '../../interfaces/book';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
